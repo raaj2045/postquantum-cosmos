a1460c04f1f16a2f54f74a8fce66a8aa7285ef65
Raaj Anand Mishra <raaj2045@gmail.com>
2025-04-03 19:12:34 +0530

    falcon key added

---
+++ crypto/keys/falcon/privkey.go
@@ -0,0 +1,69 @@
+package dillithium
+
+import (
+	"bytes"
+
+	"github.com/cloudflare/circl/sign/mldsa/mldsa44"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+)
+
+// GenPrivKey generates a new mldsa 44 private key. It uses operating system randomness.
+func GenPrivKey() (*PrivKey, error) {
+	_, prkey, err := mldsa44.GenerateKey(nil)
+	return &PrivKey{Secret: prkey.Bytes()}, err
+}
+
+// PubKey implements SDK PrivKey interface.
+func (m *PrivKey) PubKey() cryptotypes.PubKey {
+	privKey := new(mldsa44.PrivateKey)
+
+	if err := privKey.UnmarshalBinary(m.Secret); err != nil {
+		panic(err)
+	}
+
+	pubKey := privKey.Public()
+
+	edPubKey, ok := pubKey.(*mldsa44.PublicKey)
+	if !ok {
+		panic("expected mldsa44.PublicKey")
+	}
+
+	return &PubKey{Key: edPubKey.Bytes()}
+}
+
+// Type returns key type name. Implements SDK PrivKey interface.
+func (m *PrivKey) Type() string {
+	return "PrivKey(ed25519dilithium2)"
+}
+
+// Sign hashes and signs the message usign Dillithium2. Implements sdk.PrivKey interface.
+func (m *PrivKey) Sign(msg []byte) ([]byte, error) {
+	privKey := new(mldsa44.PrivateKey)
+
+	if err := privKey.UnmarshalBinary(m.Secret); err != nil {
+		panic(err)
+	}
+
+	return privKey.Scheme().Sign(privKey, msg, nil), nil
+}
+
+// Bytes serialize the private key.
+func (m *PrivKey) Bytes() []byte {
+	if m == nil {
+		return nil
+	}
+	return m.Secret
+}
+
+// Equals implements SDK PrivKey interface.
+func (m *PrivKey) Equals(other cryptotypes.LedgerPrivKey) bool {
+	if other == nil {
+		return false
+	}
+	otherKey, ok := other.(*PrivKey)
+	if !ok {
+		return false
+	}
+	// Compare the underlying secret bytes
+	return bytes.Equal(m.Secret, otherKey.Secret)
+}
---
+++ crypto/keys/falcon/pubkey.go
@@ -0,0 +1,55 @@
+package dillithium
+
+import (
+	"bytes"
+
+	"github.com/cloudflare/circl/sign/mldsa/mldsa44"
+	tmcrypto "github.com/tendermint/tendermint/crypto"
+
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+)
+
+// Bytes implements SDK PubKey interface.
+func (m *PubKey) Bytes() []byte {
+	if m == nil {
+		return nil
+	}
+	return m.Key
+}
+
+// Equals implements SDK PubKey interface.
+func (m *PubKey) Equals(other cryptotypes.PubKey) bool {
+	if other == nil {
+		return false
+	}
+	otherKey, ok := other.(*PubKey)
+	if !ok {
+		return false
+	}
+	// Compare the underlying bytes
+	return bytes.Equal(m.Key, otherKey.Key)
+}
+
+// Address implements SDK PubKey interface.
+func (m *PubKey) Address() tmcrypto.Address {
+	if m == nil {
+		return nil
+	}
+	return tmcrypto.AddressHash(m.Bytes())
+}
+
+// Type returns key type name. Implements SDK PubKey interface.
+func (m *PubKey) Type() string {
+	return "PubKey(ed25519dilithium2)"
+}
+
+// VerifySignature implements SDK PubKey interface.
+func (m *PubKey) VerifySignature(msg []byte, sig []byte) bool {
+	pubKey := new(mldsa44.PublicKey)
+
+	if err := pubKey.UnmarshalBinary(m.Key); err != nil {
+		panic(err)
+	}
+
+	return pubKey.Scheme().Verify(pubKey, msg, sig, nil)
+}
